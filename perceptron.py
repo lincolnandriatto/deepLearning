02cb2da5150381a472eb9bd765efc152fe91edcb
lincolnandriatto <lincoln.andriatto@gmail.com>
2019-04-01 08:49:06 -0300

    Create perceptron.py

---
+++ perceptron.py
@@ -0,0 +1,84 @@
+from random import random
+from math import tan
+import numpy as np
+
+X = [[0, 0], [0, 1], [1, 0], [1, 1]]
+d = [ -1, -1, -1, 1]
+W = percptrom(X, d)
+
+def perceptron(X, d):
+    N=len(X);
+    X = [[0 for x in range(N)], X];
+    ne = len(X[0]);
+    ns = len(X[0]);
+    W = [random() for i in range(ne)];
+    #Y = calc_saida(X, W);
+    erro = Y - d;
+    EQM = 1 / N * sum(sum(erro. * erro));
+    nit = 0;
+    nitmax = 10000;
+    vet_erro = EQM;
+
+    while EQM > 1e-6 & nit < nitmax:
+        nit = nit + 1;
+        dJdW = calc_grad(X, d, W, N);
+        dir = -dJdW;
+        alfa = calc_alfa(X, d, W, dir, N);
+        W = W + alfa * dir;
+        Y = calc_saida(X, W);
+        erro = Y - d;
+        EQM = 1 / N * sum(sum(erro.* erro))
+        vet_erro.push(EQM);
+
+    #plot(0: nit, vet_erro, 'linewidth', 2)
+    #xlabel('Numero de epocas')
+    #ylabel('EQM')
+    #title('Evolucao do EQM')
+
+def calc_grad(X, d, W, N):
+    Z=X*W.T();
+    Y= tan(Z);
+    erro = Y -d;
+    dJdW = 1/N*((erro.*(1-Y.*Y)).transpose()*X);
+    return djdW;
+
+def calc_saida(X, W):
+    Z= X * W.T();
+    Y= tan(Z);
+    return Y;
+
+def calc_alfa(X, d, W, dir, N):
+    alfa_l= 0;
+    alfa_u= rand;
+    Wnew = W + alfa_u*dir;
+    #%*******************************************************
+    # calcula o alfa_u positivo
+    g=calc_grad(X,d,Wnew,N);
+    h=g(:)'*dir(:);
+
+    while h<0:
+        alfa_u = 2*alfa_u;
+        Wnew = W + alfa_u*dir;
+        g=calc_grad(X,d,Wnew,N);
+        h=g(:)'*dir(:);
+
+    #**********************************************************
+    alfa_m = (alfa_l+alfa_u)/2;
+    k = ceil(log((alfa_u-alfa_l)/1.0e-5));
+    nit = 0;
+
+    while nit<k & abs(h)>1.0e-5:
+        Wnew = W + alfa_m*dir;
+        g=calc_grad(X,d,Wnew,N);
+        h=g(:)'*dir(:);
+        
+        if h>0:
+            alfa_u = alfa_m;
+        else
+            alfa_l = alfa_m;
+        
+        alfa_m = (alfa_l+alfa_u)/2;
+    end
+    
+    alfa = alfa_m;
+    return alta;
